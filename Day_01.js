268adcabd64481a1edb7f3569e84ac406bfcc3fc
Abhay Sharma <124167830+abhay-0907@users.noreply.github.com>
2024-08-05 21:33:35 +0530

    Add files via upload

---
+++ Day_01.js
@@ -0,0 +1,50 @@
+// Activity 1: Variable decalaration
+
+
+// Task 1:
+var num = 7;
+console.log(num);
+
+// Task 2:
+let string = "30 days JavaScript Challange";
+console.log(string);
+
+// Activity 2: Constant Declaration
+//Task 3:
+const boolean = true;
+console.log(boolean);
+
+//Activity 3: Data Types
+
+// Task 4:
+let no = 9;
+let str = "Abhay Sharma";
+let bool = false;
+let obj = {
+    name:"Abhay Sharma",
+    email:"example@gmail.com",
+}
+let arr = [30,31,28,29,52,53,12,365,1];
+console.log(typeof(no))
+console.log(typeof(str))
+console.log(typeof(bool))
+console.log(typeof(obj))
+console.log(typeof(arr)) // In JavaScript the array is type of object
+
+
+// Activity 4 Reassigning Variable
+//Task 5:
+let a = 100;
+// console.log(a);
+a = 239;
+console.log(a); 
+
+// Answer: Overwrite the value of "a"
+
+
+// Activity 5
+// task 6:
+const email = "example@gmailcom";
+email = 'infojs.com'
+console.log(email); 
+// Answer: TypeError: Assignment to constant variable.
