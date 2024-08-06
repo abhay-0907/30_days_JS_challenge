de5bee20554703082c3c37ca284fb9bb1193751a
Abhay Sharma <124167830+abhay-0907@users.noreply.github.com>
2024-08-06 14:00:39 +0530

    New File Upload

---
+++ Day_02.js
@@ -0,0 +1,81 @@
+// Operator
+// Activity 1: Arithmetic Operator
+
+
+// task 1:
+let first = 10;
+let second = 20.34;
+let sum = first + second;
+console.log(sum); // 30.34;
+
+// Task 2:
+let one = 453.98;
+let two = 674.78;
+console.log(one-two) // -220.79999999999995
+
+// task 3:
+
+let num1 = 9387.4;
+let num2 = 878.0;
+console.log(num1*num2) // 8242137.199999999
+
+// Task 4:
+let n1 = 45.34;
+let n2 = 9;
+console.log(n1/n2); // 5.037777777777778
+
+// task 5:
+let a = 893.23;
+let b = 48.9;
+console.log(a%b); // 13.030000000000044
+
+// Activity 2: Assignment Operator
+
+// task 6:
+let a1 = 34;
+a1+=784;
+console.log(a1)
+
+// task 7:
+
+let a2 = 98;
+a2-=34;
+console.log(a2);
+
+
+// Activity 8:  Comparison Operator
+
+let a3 = null;
+let a4 = 0;
+let a5 = 23;
+let a6 = "23"
+console.log(a3>a4);
+console.log(a3<a4);
+console.log(a5==a6);
+console.log(a5===a6);
+
+// task 9:
+let a7 = 38;
+let a8 = "38";
+console.log(a7<=a8);
+console.log(a7>=a8);
+
+
+
+
+// Activity 4: Logical Operator
+
+//// task 11:
+let age = 29;
+console.log(age>18&&age<56);
+age = 67
+console.log(age>18||age<56);
+age=0
+console.log(!age);
+
+// TASK 12:
+let number = 2323;
+console.log(number>=0?"Number is Positive":"Number is Negative");
+
+number = -1;
+console.log(number>=0?"Number is Positive":"Number is Negative");
